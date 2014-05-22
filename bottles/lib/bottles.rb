75cb6e5890c6a64b5a305719445199ac6f174f4e
Katrina Owen <katrina.owen@gmail.com>
2014-05-22 13:17:02 -0600

    Go back to DRY, name a concept

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -7,126 +7,45 @@ class Bottles
     start.downto(the_end).map {|i| verse(i)}.join("\n")
   end
 
-  def verse(num)
-    Verse.new(variant_for(num)).to_s
-  end
-
-  def variant_for(num)
-    case num
-    when 0
-      VerseVariant0.new(num)
-    when 1
-      VerseVariant1.new(num)
-    when 2
-      VerseVariant2.new(num)
-    else
-      VerseVariant.new(num)
-    end
-  end
-end
-
-class Verse
-  attr_reader :variant
-  def initialize(variant)
-    @variant = variant
-  end
-
-  def to_s
-    "#{how_many} #{containers} of beer on the wall, ".capitalize +
-    "#{how_many} #{containers} of beer.\n" +
-    "#{what_to_do}, " +
-    "#{next_how_many} #{next_containers} of beer on the wall.\n"
+  def verse(bottle_number)
+    "#{how_many(bottle_number)} #{containers(bottle_number)} of beer on the wall, ".capitalize +
+    "#{how_many(bottle_number)} #{containers(bottle_number)} of beer.\n" +
+    "#{what_to_do(bottle_number)}, " +
+    "#{how_many(bottle_number-1)} #{containers(bottle_number-1)} of beer on the wall.\n"
   end
 
   private
-
-  def what_to_do
-    variant.what_to_do
+  def what_to_do(bottle_number)
+    if bottle_number.zero?
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(bottle_number)} down and pass it around"
+    end
   end
 
-  def how_many
-    variant.how_many
+  def how_many(bottle_number)
+    if bottle_number.zero?
+      'no more'
+    elsif bottle_number < 0
+      "99"
+    else
+      "#{bottle_number}"
+    end
   end
 
-  def next_how_many
-    variant.next_how_many
+  def pronoun(bottle_number)
+    if bottle_number == 1
+      'it'
+    else
+      'one'
+    end
   end
 
-  def pronoun
-    variant.pronoun
-  end
-
-  def containers
-    variant.containers
-  end
-
-  def next_containers
-    variant.next_containers
-  end
-end
-
-class VerseVariant
-  attr_reader :num
-
-  def initialize(num)
-    @num = num
-  end
-
-  def what_to_do
-    "Take #{pronoun} down and pass it around"
-  end
-
-  def how_many
-    "#{num}"
-  end
-
-  def next_how_many
-    "#{num - 1}"
-  end
-
-  def pronoun
-    'one'
-  end
-
-  def containers
-    'bottles'
-  end
-
-  def next_containers
-    'bottles'
-  end
-end
-
-class VerseVariant0 < VerseVariant
-  def what_to_do
-    "Go to the store and buy some more"
-  end
-
-  def how_many
-    'no more'
-  end
-
-  def next_how_many
-    "99"
-  end
-end
-
-class VerseVariant1 < VerseVariant
-  def containers
-    'bottle'
-  end
-
-  def pronoun
-    'it'
-  end
-
-  def next_how_many
-    'no more'
-  end
-end
-
-class VerseVariant2 < VerseVariant
-  def next_containers
-    'bottle'
+  def containers(bottle_number)
+    if bottle_number == 1
+      'bottle'
+    else
+      'bottles'
+    end
   end
 end
